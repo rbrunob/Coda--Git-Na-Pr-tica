55ba88a708f41d4e6bb7c1720da85aead558c32a
BrunoBRamos <bruno.bragaramos@gmail.com>
2021-02-21 18:01:23 -0300

    primeiro commit

---
+++ codaí/css/index.js
@@ -0,0 +1,39 @@
+var nome = "Jair Ramos"; 
+var cargo = "Motorista"
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html")
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+
+
+function colocarNomeNoHtml(nome){
+nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo){
+     cargoHtml.innerHTML = cargo
+}
+
+function logarNome(){
+    console.log(nome)
+}
+
+function clickNoProjetos() {
+    console.log("clicou no botão projetos")
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickNoSobre(){
+    console.log("clicou no botão sobre")
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
+
+
+
